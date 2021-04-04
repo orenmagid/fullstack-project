b6b9412494190185ca6495e048cbd2e0a4741b8d
orenmagid <oren.michael.magid@gmail.com>
2021-04-04 14:07:08 -0400

    Frontend: implement favoriting functionality and display

--- frontend/src/components/Image.js
+++ frontend/src/components/Image.js
@@ -1,10 +1,40 @@
 import React from "react"
-import { Card, Grid, Image as SemanticImage } from "semantic-ui-react"
+import {
+  Card,
+  Grid,
+  Button,
+  Icon,
+  Image as SemanticImage,
+} from "semantic-ui-react"
 
-const Image = ({ id, url, description, alt_description }) => {
+const Image = ({
+  id,
+  url,
+  description,
+  alt_description,
+  buttonDisabled,
+  handleFavorite,
+}) => {
   return (
     <Grid.Column>
       <Card>
+        <Button
+          icon
+          labelPosition="left"
+          disabled={buttonDisabled}
+          onClick={() =>
+            handleFavorite({
+              unsplash_id: id,
+              url,
+              description,
+              alt_description,
+            })
+          }
+        >
+          <Icon name="heart" />
+          {buttonDisabled ? "You love this image!" : "Save to Favorites"}
+        </Button>
+
         <SemanticImage
           src={url}
           href={url}
