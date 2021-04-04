1d3d3371e547ba871d0f0668473a4f58ee53be3b
orenmagid <oren.michael.magid@gmail.com>
2021-04-04 12:10:48 -0400

    Frontend: set up basic, centered container for app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Container, Segment } from "semantic-ui-react"
+import "./App.css"
+
+function App() {
+  return (
+    <Container textAlign="center">
+      <Segment className="wrapper">
+        {/* Search input */}
+        {/* Images */}
+        {/* Favorites */}
+      </Segment>
+    </Container>
+  )
+}
+
+export default App
